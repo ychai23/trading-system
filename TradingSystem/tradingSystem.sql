624772634ff1c6357ed3ba3a0f03785de315f15b
Saisriram Gunturu <saisriram@saimail.com>
2023-04-25 11:48:28 -0400

    Update SQL
    added fields

--- TradingSystem/tradingSystem.sql
+++ TradingSystem/tradingSystem.sql
@@ -11,14 +11,18 @@ CREATE TABLE Users (
   fname VARCHAR(50),
   lname VARCHAR(50),
   email VARCHAR(100),
+  role VARCHAR(50),
   password VARCHAR(100),
-   PRIMARY KEY(userid)
+  isactive BOOLEAN,
+	PRIMARY KEY(userid)
 );
 
 CREATE TABLE Stocks (
   stockid INT auto_increment not null,
   name VARCHAR(50),
+  symbol VARCHAR(50),
   price DECIMAL(10,2),
+  isactive BOOLEAN,
   PRIMARY KEY(stockid)
 );
 
@@ -26,6 +30,7 @@ CREATE TABLE Stocks (
 CREATE TABLE userStock(
 userid INT not null,
 stockid INT not null,
+buyPrice DECIMAL(10,2),
 FOREIGN KEY (userid) REFERENCES Users(userid) ON DELETE NO ACTION,
 FOREIGN KEY (stockid) REFERENCES Stocks(stockid) ON DELETE NO ACTION,
 quantity INT,
